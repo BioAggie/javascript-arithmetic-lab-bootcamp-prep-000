af31792f798321c80379e082a531c0a266b17b3b
Jason Cole <jasonacole82@gmail.com>
2018-08-20 05:45:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function add(a,b){
   return a + b;
 }
@@ -22,3 +23,7 @@ function add(a,b){
  function preserveDecimal(n){
   return parseFloat(n);
 }
+=======
+add('2', 10)
+subtract('15', 10)
+>>>>>>> 6477d21bb65993be7418db2404069e856d45fa59
